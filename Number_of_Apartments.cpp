01144e20d0e07e1b7bf9f115f613ea88228c424a
ranveerrajput <ranveerrajput351@gmail.com>
2020-11-07 20:36:27 +0530

    done

--- Number_of_Apartments.cpp
+++ Number_of_Apartments.cpp
@@ -7,18 +7,7 @@ cin>>ntc;
 while(ntc--){
     int n;
     cin>>n;
-    int c1=0;
-    int c2=0;
-    int c3=0;
-    c1=n/7;
-    n=n/7+n%7;
-    c2=n/5;
-    n=n/5+n%5;
-    c3=n/3;
-    n=n/3+n%3;
-    if(n>0) cout<<-1<<endl;
-    else cout<<c3<<" "<<c2<<" "<<c1<<endl;
-   
-}
+    for(int i=3)
+    
 return 0;
 }
