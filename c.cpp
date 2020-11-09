3e932dea885a2025b7b1a75857a7bba0a6ffb9d6
ranveerrajput <ranveerrajput351@gmail.com>
2020-11-09 23:20:22 +0530

    done

---
+++ c.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main(){
+
+    ll ntc;
+    cin>>ntc;
+    while(ntc--){
+        ll n;
+        cin>>n;
+        ll s;
+        cin>>s;
+        ll div=10;
+        for(int i=0;i<n;i++){
+            if(s%10==0 )
+        }
+        
+
+
+    }
+    
+    return 0;
+}
