1ddba1550ebccaf3df8bdb0668e973cc0d94629e
RANVEER RAJPUT <ranveerrajput351@gmail.com>
2021-01-11 23:17:26 +0530

    practice

---
+++ magicnumbers.cpp
@@ -0,0 +1,118 @@
+// Problem Link - http://codeforces.com/problemset/problem/320/A
+/* By ranveerrajput*/
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll 	        long long int
+#define ld		long double
+#define mod             1000000007
+#define inf             1e18
+#define endl            "\n"
+#define pb 	        push_back
+#define vi              vector<ll>
+#define vs		vector<string>
+#define pii             pair<ll,ll>
+#define ump             unordered_map
+#define mp 	        make_pair
+#define pq_max          priority_queue<ll>
+#define pq_min          priority_queue<ll,vi,greater<ll> >
+#define all(n) 	        n.begin(),n.end()
+#define ff 	        first
+#define ss              second
+#define mid(l,r)        (l+(r-l)/2)
+#define bitc(n) 	__builtin_popcount(n)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
+template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
+template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+// void file_i_o()
+// {
+//     ios_base::sync_with_stdio(0); 
+//     cin.tie(0); 
+//     cout.tie(0);
+//     #ifndef ONLINE_JUDGE
+//         freopen("input.txt", "r", stdin);
+//         freopen("output.txt", "w", stdout);
+//     #endif
+// }
+
+// int main(int argc, char const *argv[]) {
+//     clock_t begin = clock();
+//     file_i_o();
+//     // Write your code here....
+//      cout<<"hello ranveer"<<endl;
+
+//     #ifndef ONLINE_JUDGE 
+//       clock_t end = clock();
+//       cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+//     #endif 
+
+
+
+ int main(){
+
+     string s;
+     cin>>s;
+     int pre=0,cur=s[0],prepre=0,f=0;;
+
+     for(ll i=0;i<s.size();i++){
+         if(i==1) pre=s[0];
+         if(i==2) prepre=s[0];
+         if(s[i]=='1'){
+             if(pre!=0) pre=s[i];
+             if(prepre!=0) prepre=s[i-1];
+             continue;
+         }
+         else if(s[i]=='4'){
+             if(pre=='1'){
+                 if(pre!=0) pre=s[i];
+                 if(prepre!=0) prepre=s[i-1];
+                 continue;
+             }
+             else if(pre=='4'){
+                 if(prepre=='1'){
+                     if(pre!=0) pre=s[i];
+                    if(prepre!=0) prepre=s[i-1];
+                    continue;
+
+                 }
+                 else{
+                     f=1;
+                     break;
+                 }
+             }
+             else{
+                 f=1;
+                 break;
+             }
+         }
+         else{
+             f=1;
+             break;
+         }
+
+     }
+     if(f==1) cout<<"NO"<<endl;
+     else cout<<"YES"<<endl;
+
+     
+    return 0;
+}
